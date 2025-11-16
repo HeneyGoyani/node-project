57b4b740883f75dfcdf09fc64b7bd8e0b2823d81
Heney Goyani <bharatgoyani8788@gmail.com>
2025-11-16 15:23:33 +0530

    pr-7

---
+++ project-7/config/DbConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://bharatgoyani8788_db_user:Heney2015@cluster0.hofojjp.mongodb.net/Admin-panel");
+    // await mongoose.connect("");
+    console.log("Database Connected Successfully");
+  } catch (err) {
+    console.error("Database Connection Failed:", err);
+  }
+};
+
+module.exports = dbConnect;
+ 
